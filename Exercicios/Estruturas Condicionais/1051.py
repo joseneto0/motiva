f9fbadbe11faad7c168c218868f3217e905b7279
José Neto <joseneto.jn1606@gmail.com>
2023-06-19 15:05:00 -0300

    Exercícios - For While

--- Exercicios/Estruturas Condicionais/1051.py
+++ Exercicios/Estruturas Condicionais/1051.py
@@ -5,8 +5,8 @@ elif valor <= 3000:
     imposto = (valor - 2000) * 8/100 
     print(f'R$ {imposto:.2f}')
 elif valor <= 4500:
-    imposto = (valor - 3000) * 18/100 + 80 #1000 * 8/100
+    imposto = (valor - 3000) * 18/100 + 80 #80 = 1000 * 8/100
     print(f'R$ {imposto:.2f}')
 else:
-    imposto = (valor - 4500) * 28/100 + 270 + 80 # 1500 * 18/100, 1000 * 8/100
+    imposto = (valor - 4500) * 28/100 + 270 + 80 # 270 = 1500 * 18/100, 80 = 1000 * 8/100
     print(f'R$ {imposto:.2f}')
