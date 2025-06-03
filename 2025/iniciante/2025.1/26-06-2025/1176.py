c399a8fac17b399ad7709339933a9c9ddafb7d56
José Neto <joseneto.jn1606@gmail.com>
2025-06-02 21:09:10 -0300

    melhorando o codigo do fibonnaci

--- 2025/iniciante/2025.1/26-06-2025/1176.py
+++ 2025/iniciante/2025.1/26-06-2025/1176.py
@@ -1,10 +1,8 @@
-T = int(input())
-for i in range(T):
-    fibo = [0, 1]
-    N = int(input())
-    if N > 1:
-        for c in range(2, N+1):
-            fibo.append(fibo[c-2] + fibo[c-1])
-        print(f'Fib({N}) = {fibo[N]}')
-    else:
-        print(f'Fib({N}) = {fibo[N]}')
+fibo = [0, 1]
+for i in range(2, 61):
+    fibo.append(fibo[i-1] + fibo[i - 2])
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    print(f"Fib({n}) = {fibo[n]}")
