c9d9ba595ab5e808e23914939e02ab2f001d1ba4
José Neto <joseneto.jn1606@gmail.com>
2025-09-29 19:14:40 -0300

    facilitando visualizacao

--- 2025/intermediario I/2025.2/29-09-2025/even-odds.py
+++ 2025/intermediario I/2025.2/29-09-2025/even-odds.py
@@ -1,11 +1,11 @@
 n, k = map(int, input().split())
-aux = 0 # variavel auxiliar para guardar a quantidade de impares
+quantidade_impares = 0 # variavel auxiliar para guardar a quantidade de impares
 if n % 2 == 0: 
-    aux = n // 2 
+    quantidade_impares = n // 2 
 else: 
-    aux = (n + 1) // 2 
-    
-if k <= aux: # esta na parte dos impares
+    quantidade_impares = (n + 1) // 2 
+
+if k <= quantidade_impares: # esta na parte dos impares
     print(k * 2 - 1)
 else: # esta na parte dos pares
-    print((k - aux) * 2)
+    print((k - quantidade_impares) * 2)
